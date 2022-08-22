ac463c66ad87a7188d1eb6788ffd356852dbe3f4
Vitomir2 <vitomirpavlov@gmail.com>
2022-08-22 15:34:38 +0300

    modify the code for the x509 certificates (working self-signed x509) and modify the arduino bearssl config;

--- source/auth-x509-certs-arduino-sketches/authentication-x509-certificates-2/authentication-x509-certificates-2.ino
+++ source/auth-x509-certs-arduino-sketches/authentication-x509-certificates-2/authentication-x509-certificates-2.ino
@@ -12,6 +12,8 @@
    GNU General Public License v3.0
 */
 
+#include <ArduinoJson.h>
+#include <ArduinoJson.hpp>
 #include <ArduinoBearSSL.h>
 #include <ArduinoECCX08.h>
 #include <utility/ECCX08SelfSignedCert.h>
@@ -30,6 +32,7 @@ const int certSlot    = 8;  // Crypto chip slot to pick the certificate from
 
 const char* ssid       = SECRET_WIFI_SSID;
 const char* pass       = SECRET_WIFI_PASS;
+const char* broker     = SECRET_BROKER;
 const char* DPS_broker = SECRET_DPS_BROKER;
 const String idScope   = SECRET_ID_SCOPE;
 const String device_id = SECRET_DEVICE_ID;
@@ -79,7 +82,17 @@ void setup() {
     // and set the hardcoded certificate as accompanying public certificate.
     sslClient.setEccSlot(
       keySlot,
-      CLIENT_CERT);
+      CLIENT_PUBLIC_CERT);
+    
+    // br_x509_certificate x509cert = {
+    //  CLIENT_CERT,
+    //  sizeof(CLIENT_CERT) - 1
+    // };
+
+    // Set the X509 certificate
+    // sslClient.setEccCert(x509cert);
+
+    // sslClient.setKey(CLIENT_KEY, CLIENT_CERT);
   }
 
   /*
@@ -95,12 +108,12 @@ void setup() {
   // Set the username to "<idScope>/registrations/<registrationId>/api-version=2019-03-31"
   // String username = broker + "/registrations/" + device_id + "/api-version=2019-03-31";
   String username;
-  username += idScope;
-  username += "/registrations/";
+  username += broker;
+  username += "/";
   username += device_id;
-  username += "/api-version=2019-03-31";
+  username += "/?api-version=2018-06-30";
 
-  // Set an empty password (because of X.509 authentication instead of SAS token)
+  // Set an empty password if you want to use X.509 authentication, otherwise set the SAS token connection string
   String password = "";
 
   // Authenticate the MQTT Client
@@ -110,11 +123,79 @@ void setup() {
   mqttClient.onMessage(onMessageReceived);
 }
 
+void publishMessage() {
+  Serial.println("Publishing message");
+  
+  const int capacity = JSON_ARRAY_SIZE(10) + 10*JSON_OBJECT_SIZE(2)+ JSON_OBJECT_SIZE(3) + 280;     // Calculation of the JSON doc size, as explained in the documentation
+
+  /*  
+  Here is where you should write the body of your message. In this example, the JSON doc is purposely longer than 256 characters, to highlight the issue.
+  */
+
+  StaticJsonDocument<capacity> doc;
+  doc["topic"] = "messageTopic";  
+  doc["deviceId"] = device_id;
+  JsonArray data = doc.createNestedArray("data");
+  JsonObject data_0 = data.createNestedObject();
+  data_0["label"] = "Ankara";
+  data_0["state"] = true;
+  JsonObject data_1 = data.createNestedObject();
+  data_1["label"] = "Beirut";
+  data_1["state"] = true;
+  JsonObject data_2 = data.createNestedObject();
+  data_2["label"] = "Cincinnati";
+  data_2["state"] = false;
+  JsonObject data_3 = data.createNestedObject();
+  data_3["label"] = "Detroit";
+  data_3["state"] = false;
+  JsonObject data_4 = data.createNestedObject();
+  data_4["label"] = "Eindhoven";
+  data_4["state"] = true;
+  JsonObject data_5 = data.createNestedObject();
+  data_5["label"] = "Fresno";
+  data_5["state"] = false;
+  JsonObject data_6 = data.createNestedObject();
+  data_6["label"] = "Genoa";
+  data_6["state"] = false;
+  JsonObject data_7 = data.createNestedObject();
+  data_7["label"] = "Huddersfield";
+  data_7["state"] = true;
+  JsonObject data_8 = data.createNestedObject();
+  data_8["label"] = "Istanbul";
+  data_8["state"] = true;
+  JsonObject data_9 = data.createNestedObject();
+  data_9["label"] = "Jakarta";
+  data_9["state"] = false;
+  
+
+  //   DEBUG - serialize the document in the serial monitor
+  //   serializeJson(doc, Serial);
+  //   Serial.println(" ");
+    
+  char payload[1024]; // length of the char buffer that contains the JSON file, concretely the number of characters included in one message
+  size_t payloadSize = serializeJson(doc, payload);
+  
+  //   DEBUG - write the size of the serialized document
+  //   Serial.print("json size:");
+  //   Serial.println(payloadSize);
+    
+  // send message, the Print interface can be used to set the message contents
+  mqttClient.beginMessage("devices/" + device_id + "/messages/events/", static_cast<unsigned long>(payloadSize));
+  mqttClient.print(payload);
+  mqttClient.endMessage();
+  /*  
+  To replicate the issue, uncomment the following 3 lines and comment the 3 above. This way you'll only be able to send MQTT messages smaller than 256 Bytes.
+  */
+  //  mqttClient.beginMessage("devices/" + device_id + "/messages/events/");
+  //  serializeJson(doc, mqttClient);
+  //  mqttClient.endMessage();
+}
+
 void loop() {
   // ================ LOOP FUNCTION ================
   // Select the MQTT topic to subscribe to. It is a default value for DPS.
-  String sub_topic = "$dps/registrations/res/#";
-  // String sub_topic = "devices/" + device_id + "/messages/devicebound/#";
+  // String sub_topic = "$dps/registrations/res/#";
+  String sub_topic = "devices/" + device_id + "/messages/devicebound/#";
 
   // Connect to WiFi
   if (WiFi.status() != WL_CONNECTED) {
@@ -125,6 +206,13 @@ void loop() {
   if (!mqttClient.connected()) {
     connectMQTT(sub_topic);
   }
+
+  // poll for new MQTT messages and send keep alives
+  // mqttClient.poll();
+
+  // publishMessage();
+
+  // delay(10000); //  publish a message roughly every 10 seconds
 }
 
 unsigned long getTime() {
@@ -150,16 +238,19 @@ void connectWiFi() {
 
 void connectMQTT(String topic) {
   Serial.print("Attempting to connect to MQTT broker: ");
-  Serial.print(DPS_broker);
+  Serial.print(broker);
   Serial.println(" ");
 
-  while (!mqttClient.connect(DPS_broker, 8883)) {
+  long int start_time = millis();
+  while (!mqttClient.connect(broker, 8883)) {
     delay(5000);
     // Failed, retry
     Serial.print("connectError: ");
     Serial.println(mqttClient.connectError());
+    start_time = millis();
   }
-  Serial.println();
+  long int end_time = millis();
+  Serial.print("Execution time for the connection: "); Serial.print(end_time-start_time); Serial.println(" milliseconds");
 
   Serial.println("You're connected to the MQTT broker");
   Serial.println();
